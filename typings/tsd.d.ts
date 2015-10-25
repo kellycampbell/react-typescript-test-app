52ab908b29750f1f470ea768b20befd546c6c34f
Kelly Campbell <kelly.a.campbell@gmail.com>
2015-10-25 15:36:48 -0400

    Checking typings in per recommendation from https://github.com/DefinitelyTyped/tsd#notes
    This is due to github api limits possibly causing issues downloading
    type files. Also there seems to be a bug in tsd install that causes it
    not to get redux or react-redux with the --save flag.

---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="react-dom/react-dom.d.ts" />
+/// <reference path="react/react.d.ts" />
+/// <reference path="redux/redux.d.ts" />
+/// <reference path="react-redux/react-redux.d.ts" />
+/// <reference path="../node_modules/immutable/dist/immutable.d.ts" />
